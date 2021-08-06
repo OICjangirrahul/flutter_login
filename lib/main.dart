1238d58939e67539c48df267f85a454acc42124f
rahul <rahuljangir139@gmail.com>
2021-08-06 20:54:30 +0900

    scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
-sdjjklzkjlzklzkljljkcjklcxjklxc
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        
+        appBar: AppBar(),
+        drawer: Drawer(),
+        body: Center(
+          child: Container(
+            child: Text("hello",
+                style: TextStyle(fontSize: 30, color: Colors.brown)),
+          ),
+        ),
+      ),
+    );
+  }
+}
